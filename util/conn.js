2aaa309ca2596e6ebfdc98cc42ea629a7ab9339b
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2021-09-05 10:31:24 +0530

    environment variables

--- util/conn.js
+++ util/conn.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    // host: process.env.host,
-    // user: process.env.user,
-    // password: process.env.password,
-    // database: process.env.database
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
 
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'swin'
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'root',
+    // database: 'swin'
 
 });
 
