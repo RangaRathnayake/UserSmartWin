0b93a8d3a18500dc1285bab295faddac4cae2cfc
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2022-07-01 00:56:34 +0530

    update

--- util/conn.js
+++ util/conn.js
@@ -2,11 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.host,
-    port: process.env.port,
     user: process.env.user,
     password: process.env.password,
-    database: process.env.database,
-   
+    database: process.env.database
 
     // host: 'localhost',
     // user: 'root',
